31264da3ba408c65ca31a7f65d9b196d14636a99
ayush-pandey007 <70948759+ayush-pandey007@users.noreply.github.com>
2021-06-27 23:41:36 +0530

    done with business logic

--- main.js
+++ main.js
@@ -10,36 +10,39 @@ currLife.innerHTML = life;
 let bgmusic = new Audio();
 bgmusic.src = "./audio/backgroundmusic.mp3";
 bgmusic.play();
-bgmusic.volume = 0.6;
-// let brickcollide = new Audio();
-// brickcollide = "./audio/collide.mp3";
-// brickcollide.volume = 0.9;
+// bgmusic.volume = 0.6;
 
 let canvas = document.getElementById("mycanvas");
 let ctx = canvas.getContext("2d");
 let ballRadius = 10;
 
-//bricks-area
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-
-
-//ball Movement
-let dx = 2;
-let dy = -2;
-
 //create the paddle
 let paddleHeight = 5;
 let paddleWidth = 100;
 
-//starting point of paddle
-let paddleX = (canvas.width - paddleWidth) / 2;
+// paddle
+const paddle = {
+  x: canvas.width / 2 - paddleWidth / 2,
+  y: canvas.height - paddleHeight,
+  width: paddleWidth,
+  height: paddleHeight,
+  dx: 5,
+};
+//Ball
+let ball = {
+  x: canvas.width / 2,
+  y: paddle.y - ballRadius,
+  radius: ballRadius,
+  speed: 4,
+  dx: 3,
+  dy: -3,
+};
 
-//holding variables for right and left arrows on keyboard
+//setting variables
 let rightPressed = false;
 let leftPressed = false;
 
-//bricks structure
+// bricks structure
 let brickRowCount = 6;
 let brickColumnCount = 8;
 let brickWidth = 71;
@@ -48,13 +51,12 @@ let brickPadding = 4;
 let brickOffsetTop = 2;
 let brickOffsetLeft = 2;
 
-//score for broken bricks
+//score
 let score = 0;
 
-//arrays for the bricks
+//array
 let bricks = [];
-
-//setting the loop for rows and cols
+// loop for bricks
 for (i = 0; i < brickColumnCount; i++) {
   bricks[i] = [];
   for (j = 0; j < brickRowCount; j++) {
@@ -62,21 +64,19 @@ for (i = 0; i < brickColumnCount; i++) {
     bricks[i][j] = { x: 0, y: 0, status: 1 };
   }
 }
-
-//track the user actions like mouse movement or keypad arrows < >
+//user actions
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
 
-//Anchor paddle movement to mouse movement
+// mouse movement
 function mouseMoveHandler(e) {
   var relativeX = e.clientX - canvas.offsetLeft;
   if (relativeX > 0 && relativeX < canvas.width) {
-    paddleX = relativeX - paddleWidth / 2;
+    paddle.x = relativeX - paddleWidth / 2;
   }
 }
-
-//movement through keypad < > arrows
+// arrows
 function keyDownHandler(e) {
   if (e.keyCode === 39) {
     rightPressed = true;
@@ -94,22 +94,20 @@ function keyUpHandler(e) {
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI * 2); //centered at (x,y) position with radius r = ballRadius starting at 0 = startAngle, ending at Math.PI*2 = endAngle (in Radians)
+  ctx.arc(ball.x, ball.y, ballRadius, 0, Math.PI * 2); //centered at (x,y) position with radius r = ballRadius starting at 0 = startAngle, ending at Math.PI*2 = endAngle (in Radians)
   ctx.fillStyle = "#fff";
   ctx.fill();
   ctx.closePath();
 }
-
-//Create a function to create the paddle
+//DRAW paddle
 function drawPaddle() {
   ctx.beginPath();
-  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight); //centered at (x,y) position with radius r = ballRadius starting at 0 = startAngle, ending at Math.PI*2 = endAngle (in Radians)
+  ctx.rect(paddle.x, paddle.y, paddle.width, paddle.height); //centered at (x,y) position with radius r = ballRadius starting at 0 = startAngle, ending at Math.PI*2 = endAngle (in Radians)
   ctx.fillStyle = "#0e9aa7";
   ctx.fill();
   ctx.closePath();
 }
-
-//Create a function to draw the bricks
+// draw bricks
 function drawBricks() {
   for (c = 0; c < brickColumnCount; c++) {
     for (r = 0; r < brickRowCount; r++) {
@@ -127,31 +125,49 @@ function drawBricks() {
     }
   }
 }
-//Create function to keep track of score
+// score
 function drawScore() {
   ctx.font = "1.7rem Arial";
   ctx.fillStyle = "#fff";
   currScore.innerHTML = score;
 }
+// RESET THE BALL
+function resetBall() {
+  ball.x = paddle.x + 50;
+  ball.y = paddle.y - ballRadius;
+  ball.dx = 3 * (Math.random() * 2 - 1);
+  ball.dy = -3;
+  // paddle.x = canvas.width / 2 - paddleWidth / 2;
+  // paddle.y = paddle.y - ballRadius;
+}
+function ballPaddleCollision() {
+  // CHECK WHERE THE BALL HIT THE PADDLE
+  let collidePoint = ball.x - (paddle.x + paddle.width / 2);
 
-//Collision dections for the bricks
+  // NORMALIZE THE VALUES
+  collidePoint = collidePoint / (paddle.width / 2);
+
+  // CALCULATE THE ANGLE OF THE BALL
+  let angle = (collidePoint * Math.PI) / 3;
+
+  ball.dx = ball.speed * Math.sin(angle);
+  ball.dy = -ball.speed * Math.cos(angle);
+}
+//Collision detections
 function collisionDetection() {
   for (c = 0; c < brickColumnCount; c++) {
     for (r = 0; r < brickRowCount; r++) {
       let b = bricks[c][r];
       if (b.status === 1) {
         if (
-          x > b.x &&
-          x < b.x + brickWidth &&
-          y > b.y &&
-          y < b.y + brickHeight
+          ball.x > b.x &&
+          ball.x < b.x + brickWidth &&
+          ball.y > b.y &&
+          ball.y < b.y + brickHeight
         ) {
-          dy = -dy;
+          ball.dy = -ball.dy;
           b.status = 0;
           score++;
-          // brickcollide.play();
-          // main.volume -= 0.01;
-          // brickcollide.pause();
           // console.log("Score is Incres");
           // currScore.innerHTML = score;
           if (score === brickRowCount * brickColumnCount) {
@@ -163,41 +179,33 @@ function collisionDetection() {
     }
   }
 }
-
-//changing position
-
-// Main Function
 function draw() {
-  //clear each instance of the canvas so a new circle can be drawn
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawScore();
   drawBricks();
   drawBall();
   drawPaddle();
   collisionDetection();
-
   //Calculate collision detections
   //left and right walls
-
-  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-    dx = -dx;
+  if (
+    ball.x + ball.dx > canvas.width - ballRadius ||
+    ball.x + ball.dx < ballRadius
+  ) {
+    ball.dx = -ball.dx;
   }
   //top walls
-  if (y + dy < ballRadius) {
-    dy = -dy;
-  } else if (y + dy > canvas.height - ballRadius) {
+  if (ball.y + ball.dy < ballRadius) {
+    ball.dy = -ball.dy;
+  } else if (ball.y + ball.dy > canvas.height - ballRadius) {
     //detect paddle hits
-    if (x > paddleX && x < paddleX + paddleWidth) {
-      dy = -dy;
-    }
-    //if no paddle hit, body of canvas is hit ==> game over
-    else {
-      //   alert("GAME OVER!! Press OK to Play Again");
-
+    if (ball.x > paddle.x && ball.x < paddle.x + paddleWidth) {
+      ball.dy = -ball.dy;
+      ballPaddleCollision();
+    } else {
       life -= 1;
+      resetBall();
       currLife.innerHTML = life;
-      drawBall();
-      drawPaddle();
       if (life == 0) {
         currLife.innerHTML = 0;
         bgmusic.pause();
@@ -210,27 +218,23 @@ function draw() {
       }
     }
   }
-
   //bottom wall
-  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-    dy = -dy;
+  if (
+    ball.y + ball.dy > canvas.height - ballRadius ||
+    ball.y + ball.dy < ballRadius
+  ) {
+    ball.dy = -ball.dy;
   }
-
-  //Make paddle move
-  if (rightPressed && paddleX < canvas.width - paddleWidth) {
-    paddleX += 7;
-  } else if (leftPressed && paddleX > 0) {
-    paddleX -= 7;
+  // paddle movement
+  if (rightPressed && paddle.x < canvas.width - paddleWidth) {
+    paddle.x += 7;
+  } else if (leftPressed && paddle.x > 0) {
+    paddle.x -= 7;
   }
-  //Making the ball move
-  x += dx; //update x movement every frame
-  y += dy; //update y movement every frame
+  //ball movement
+  ball.x += ball.dx; //update x movement every frame
+  ball.y += ball.dy; //update y movement every frame
 }
 
 //Create an infinite loop that creates the ball
-//paints the ball on the canvas every 10 milliseconds.
-
-let myvar = setInterval(draw, 8);
-
-//life jab kam hi rhai
-//audio
+let myvar = setInterval(draw, 10);
