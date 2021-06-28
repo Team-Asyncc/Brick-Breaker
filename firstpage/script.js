9cf3d032810462ee5f98903c40c6a813bbef8e0f
mohd baqer haider <mohd.b.haider000786@gmail.com>
2021-06-28 12:38:34 +0530

    fixed the bugs

---
+++ firstpage/script.js
@@ -0,0 +1,24 @@
+let mute = document.getElementById('mute')
+let unmute = document.getElementById('unmute')
+let sound = document.querySelector('.sound-icon')
+// console.log(sound);
+mute.style.display = 'none'
+let btnState = true
+sound.addEventListener('click', function () {
+  console.log(btnState)
+  if (btnState) {
+    mute.style.display = 'block'
+    unmute.style.display = 'none'
+    btnState = false
+  } else {
+    mute.style.display = 'none'
+    unmute.style.display = 'block'
+    btnState = true
+  }
+})
+
+playButton = document.querySelector('.play-button')
+// console.log(playButton)
+playButton.addEventListener('click', () => {
+  location.href = '../index.html'
+})
